de7c640277788f269dcf9becbef0824f0539d60c
ONJoseph <on.josephinfo@gmail.com>
2023-03-14 23:09:16 +0100

    Fixed typo error

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,7 +2,7 @@ require_relative 'base_decorator'
 
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    if suer.length > 10
+    if super.length > 10
       super[0...10]
     else
       super
