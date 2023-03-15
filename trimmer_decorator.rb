8f118eab34766759dca0fb1e8fffbd3a24136ee0
ONJoseph <on.josephinfo@gmail.com>
2023-03-15 20:40:59 +0100

    Add @nameable.correct_name to 10 characters max

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -3,7 +3,7 @@ require_relative 'base_decorator'
 class TrimmerDecorator < BaseDecorator
   def correct_name
     if super.length > 10
-      super[0...10]
+      super[0...9]
     else
       super
     end
