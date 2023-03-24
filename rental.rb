9e0538485e452fe74422752947fb495a970f7b0b
ONJoseph <on.josephinfo@gmail.com>
2023-03-24 20:22:14 +0100

    refactored class codes

--- rental.rb
+++ rental.rb
@@ -5,9 +5,9 @@ class Rental
   def initialize(date, book, person)
     @date = date
     @person = person
-    person['rentals'] = self
+    person.rentals << self
 
     @book = book
-    book['rentals'] = self
+    book.rentals << self
   end
 end
